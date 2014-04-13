695f40b3893cbf26786ec1f7b7ec580c62684bb7
Antoni Orfin <aorfin@imagin.pl>
2014-04-13 22:07:09 +0200

    Byte Extractor

--- ComssService/project/byte_extractor/byte_extractor_service.py
+++ ComssService/project/byte_extractor/byte_extractor_service.py
@@ -3,25 +3,20 @@
 
 from ComssService.service.sync import SyncService
 from ComssService.ServiceController import ServiceController
+import re
 
-class MultiplierService(SyncService):
+regex = '([(\d\.)]+) - - \[(.*?)\] "([A-Z]*?) (.*?)" (\d+) (\d+) "(.*?)" "(.*?)"'
+
+class ByteExtractorService(SyncService):
 
     def run(self):
-        while True:  # będzie się wykonywać tak długo, jak będzie działać usługa
-            received_dict = self.read_object('1')  # odczytaj wejsciowy slownik
-#             print 'GOT:', received_dict
-            curr_params = self.get_parameters()
-            value_to_multiply = curr_params['value_to_multiply']
-            if value_to_multiply in received_dict:
-                received_dict[value_to_multiply] *= curr_params['multiply_by']
-
-
-            self.send_object('2', received_dict)  # na wyjście o id '2' wyślij przetworzony slownik
+        while True:
+            data = self.read('1')
+            byte = re.match(regex, data).groups()[5]
+            self.send('2', byte)
 
 if __name__ == '__main__':
     # Uruchomienie usługi:
-    desc_file_name = 'address_extractor_service.xml'
-    s = ServiceController(MultiplierService, desc_file_name)
+    desc_file_name = 'byte_extractor_service.xml'
+    s = ServiceController(ByteExtractorService, desc_file_name)
     s.start()
-
-    # teraz wystarczy w kosoli uruchomić ten skrypt, aby usługa zaczęła działać
--- ComssService/project/byte_extractor/receiver.py
+++ ComssService/project/byte_extractor/receiver.py
@@ -3,13 +3,14 @@
 
 from ComssService.dev.control import DevServiceController
 
-desc_file_name = 'address_extractor_service.xml'
+desc_file_name = 'byte_extractor_service.xml'
 
 controller = DevServiceController(desc_file_name)
 try:
     while True:
-        print controller.read_object('2')
+        print controller.read_data('2')
 except:
     pass
 finally:
     controller.close_all_connections()
+	
