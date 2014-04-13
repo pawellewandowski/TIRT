c9637c0c3bcea71cf3bcd0b37a66f1ade43c5409
Antoni Orfin <aorfin@imagin.pl>
2014-04-13 22:07:23 +0200

    Http Code Extractor

--- ComssService/project/http_extractor/sender.py
+++ ComssService/project/http_extractor/sender.py
@@ -2,32 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from ComssService.dev.control import DevServiceController
-import random
-import time
+import fileinput
 
-desc_file_name = 'address_extractor_service.xml'
+desc_file_name = 'http_extractor_service.xml'
 controller = DevServiceController(desc_file_name)
 
-UPDATE_PARAMS_INTERVAL = 20
-
 try:
-    iters_to_update = UPDATE_PARAMS_INTERVAL
-    while True:
-        iters_to_update -= 1
-        if iters_to_update <= 0:
-            new_params = {'multiply_by':random.randint(0, 3),
-                          'value_to_multiply': random.choice(['a', 'b', 'c'])}
-            controller.update_params(new_params)
-            print 'PARAMS UPDATED', new_params
-            iters_to_update = UPDATE_PARAMS_INTERVAL
-
-        dict_to_send = {'a': random.randint(0, 100),
-                        'b': random.randint(0, 100),
-                        'c': random.randint(0, 100),
-                        }
-        controller.send_object('1', dict_to_send)
-        print "SENT:", dict_to_send
-        time.sleep(0.5)
+    for line in fileinput.input(['../servers_emulator/data/http_server.log']):
+        controller.send_data('1', line)
+        print line
 except:
     raise
 finally:
