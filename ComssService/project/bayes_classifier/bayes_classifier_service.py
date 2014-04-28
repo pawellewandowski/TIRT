66d041daffac7c3b893185df18e6aa1c7386bd5b
Mateusz Giżycki <mat20041@gmail.com>
2014-04-28 14:33:14 +0200

    nauczony bayes

--- ComssService/project/bayes_classifier/bayes_classifier_service.py
+++ ComssService/project/bayes_classifier/bayes_classifier_service.py
@@ -9,13 +9,13 @@ from numpy.linalg import *
 class MultiplierService(SyncService):
 
     def __init__(self):
-        self.mi = matrix.matrix([0, 0, 0, 0, 0, 0]).transpose()
-        self.cov = matrix.matrix([[1, 0, 0, 0, 0, 0],
-                                  [0, 1, 0, 0, 0, 0],
-                                  [0, 0, 1, 0, 0, 0],
-                                  [0, 0, 0, 1, 0, 0],
-                                  [0, 0, 0, 0, 1, 0],
-                                  [0, 0, 0, 0, 0, 1]])
+        self.mi = matrix.matrix([257.3289, 1891.4641, 257.3412, 1891.4578, 257.3534, 1891.0688]).transpose()
+        self.cov = matrix.matrix([[2735.1732, -105164.6553, 1394.3363, -49252.3400, 1444.7696, -55889.5886],
+                                  [-105164.6553, 804017063.0871, -79963.4768, 9576227.3922, -76743.0733, 5355152.3607],
+                                  [1394.3363, -79963.4768, 2735.0429, -105187.3935, 1394.2058, -49253.1383],
+                                  [-49252.3400, 9576227.3922, -105187.3935, 804017086.7348, -79986.2150, 9576954.0862],
+                                  [1444.7696, -76743.0733, 1394.2058, -79986.2150, 2734.9122, -105188.1871],
+                                  [-55889.5886, 5355152.3607, -49253.1383, 9576954.0862, -105188.1871, 804017269.7513]]).transpose()
         self.queue = [0, 0, 0, 0, 0, 0]
 
     def run(self):
@@ -28,7 +28,7 @@ class MultiplierService(SyncService):
                 continue
             http_code = self.last_code
             http_content_length = self.last_content_length
-            self.queue = [http_code, http_content_length] + self.queue[0:3]
+            self.queue = self.queue[2:5] + [http_code, http_content_length]
             vect = matrix.matrix(self.queue).transpose()
             probability = (1/(6.2831853071795862**3 * det(self.cov)**0.5))*exp(-0.5*(vect-self.mi).transpose()*inv(self.cov)*(vect-self.mi))
 #             print 'GOT:', received_dict
